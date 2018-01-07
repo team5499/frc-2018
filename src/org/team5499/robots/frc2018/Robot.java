7223f7a08ecb3efb834749156e7f4e3c92e25384
Andy Cate <and164v@gmail.com>
2018-01-07 08:59:30 -0800

    Initial code - deploy is still having issues

---
+++ src/org/team5499/robots/frc2018/Robot.java
@@ -0,0 +1,43 @@
+package org.team5499.robots.frc2018;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot {
+
+    public Robot() {
+        System.out.println("Hello, world!");
+    }
+
+
+    @Override
+    public void robotInit() {
+    }
+
+    @Override
+    public void robotPeriodic() {
+    }
+
+    @Override
+	public void disabledInit() {
+    }
+    
+	@Override
+	public void disabledPeriodic() {
+    }
+
+    @Override
+    public void autonomousInit() {
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+    }
+
+    @Override
+    public void teleopInit() {
+    }
+
+    @Override
+    public void teleopPeriodic() {
+    }
+}
