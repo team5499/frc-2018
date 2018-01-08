88e2ceb54071f48cd5a9126d9ea749c7bca6bc0c
Ben Scholar <bens@digitalman.com>
2018-01-07 22:53:11 -0800

    implemented basic json parsing

--- src/org/team5499/robots/frc2018/Reference.java
+++ src/org/team5499/robots/frc2018/Reference.java
@@ -1,8 +1,23 @@
 package org.team5499.robots.frc2018;
 
+import java.io.FileReader;
+
 import org.team5499.robots.frc2018.subsystems.Inputs.DriverControlMethod;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class Reference {
+
+    private static final String VAR_FILE_PATH = "/home/lvuser/vars.json";
+    private static JsonReader jReader;
+    private static JsonParser jParser;
+    private static JsonElement jElement;
+    private static JsonObject jObject;
 
     // important vars
     public static final DriverControlMethod driveMethod = DriverControlMethod.CONTROLLER;
@@ -33,5 +48,33 @@ public class Reference {
     public static final int THROTTLE_PORT = 3;
 
     //pid consts
+    public static double kP = 0;
+    public static double kI = 0;
+    public static double kD = 0;
+
+    public static void initPIDVars() {
+        try {
+            jReader = new JsonReader(new FileReader(VAR_FILE_PATH));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        jParser = new JsonParser();
+        jElement = jParser.parse(jReader);
+        jObject = jElement.getAsJsonObject();
+
+        kP = jObject.get("kP").getAsDouble();
+        kI = jObject.get("kI").getAsDouble();
+        kD = jObject.get("kD").getAsDouble();
+
+        System.out.println("kP: " + kP);
+        System.out.println("kI: " + kI);
+        System.out.println("kD: " + kD);
+        
+        SmartDashboard.putNumber("pvalue", Reference.kP);
+        SmartDashboard.putNumber("ivalue", Reference.kI);
+        SmartDashboard.putNumber("dvalue", Reference.kD);
+
+    }
 
 }
