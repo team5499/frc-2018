6f4751e3616a2741f7302afaf519f215396ac7a9
andycate <and164v@gmail.com>
2018-01-13 15:13:38 -0800

    Temp

--- src/org/team5499/robots/frc2018/Robot.java
+++ src/org/team5499/robots/frc2018/Robot.java
@@ -1,17 +1,21 @@
 package org.team5499.robots.frc2018;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.NidecBrushless;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import java.io.File;
 
 public class Robot extends IterativeRobot {
 
     public Robot() {
         System.out.println("Hello, world!");
+        TalonSRX t = new TalonSRX(2);
+        t.set(0);
     }
 
 
     @Override
     public void robotInit() {
-        System.out.println("Hello, world!");
     }
 
     @Override
