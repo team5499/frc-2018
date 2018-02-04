30238c452172dc148a0f5624e48d79d1aa904d4b
andycate <and164v@gmail.com>
2018-02-04 14:07:38 -0800

    Changing threading

--- src/org/team5499/robots/frc2018/subsystems/Subsystems.java
+++ src/org/team5499/robots/frc2018/subsystems/Subsystems.java
@@ -3,8 +3,4 @@ package org.team5499.robots.frc2018.subsystems;
 import org.team5499.robots.frc2018.subsystems.*;
 
 public class Subsystems {
-<<<<<<< HEAD
-=======
-    public static Dashboard dash = new Dashboard();
->>>>>>> 538cedb748633f65b9bcb2a000ec70da01bec85e
 }
