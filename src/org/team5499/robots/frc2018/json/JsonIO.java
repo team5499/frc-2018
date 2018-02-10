39f5b5a3eae4ee47a1f5138f935a97a5a7cdd20c
Ben Scholar <bens@digitalman.com>
2018-02-10 14:29:43 -0800

    fixed JavaIO

--- src/org/team5499/robots/frc2018/json/JsonIO.java
+++ src/org/team5499/robots/frc2018/json/JsonIO.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.BufferedReader;
 
 import org.team5499.robots.frc2018.Reference;
 
@@ -18,11 +19,11 @@ import com.google.gson.stream.MalformedJsonException;
 public class JsonIO {
 
     private static final String FILE_PATH = "/home/lvuser/vars.json";   
-    private static File file = new File(FILE_PATH);
+    private static File file;
     private static Gson gson;
 
-
     static {
+        file = new File(FILE_PATH);
         try {
             gson = new Gson();
         } catch(Exception e) {
@@ -31,14 +32,19 @@ public class JsonIO {
     }
 
     public static void updateReference(){
-        
+        FileReader fr = null;
+        BufferedReader br = null;
         try {
-            FileReader fr = new FileReader(file);
-            char[] data = new char[(int) file.length()];
-            String json = new String(data);
+            fr = new FileReader(FILE_PATH);
+            br = new BufferedReader(fr);
+            String json = "";
+            String currentLine;
+            while((currentLine = br.readLine()) != null) {
+                json += currentLine;
+            }
             System.out.println(json);
-            // Reference ref = gson.fromJson(json, Reference.class);
-            // Reference.setInstance(ref);
+            Reference ref = gson.fromJson(json, Reference.class);
+            Reference.setInstance(ref);
             System.out.println("New Reference Instance");
         } catch(Exception e) {
             e.printStackTrace();
