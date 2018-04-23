8575877dc3c42d45ea2969e0ec5a1713f99b448d
andycate <and164v@gmail.com>
2018-04-22 20:18:07 -0500

    Time tester

--- src/org/team5499/robots/frc2018/Robot.java
+++ src/org/team5499/robots/frc2018/Robot.java
@@ -4,15 +4,30 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 
 public class Robot extends TimedRobot {
+    Timer timer;
+    Timer outputTimer;
     public Robot() {
+        super.setPeriod(0.01);
+        timer = new Timer();
+        outputTimer = new Timer();
     }
 
     @Override
     public void robotInit() {
+        outputTimer.start();
     }
 
     @Override
     public void robotPeriodic() {
+        if(outputTimer.get() > 0.5) {
+            System.out.println(timer.get());
+            outputTimer.stop();
+            outputTimer.reset();
+            outputTimer.start();
+        }
+        timer.stop();
+        timer.reset();
+        timer.start();
     }
 
     @Override
