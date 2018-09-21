2d8f2e31191c93909cc410112cb7f97066b4aca6
andycate <and164v@gmail.com>
2018-09-21 10:17:17 -0700

    Added missing testInit override

--- src/org/team5499/robots/frc2018/Robot.java
+++ src/org/team5499/robots/frc2018/Robot.java
@@ -4,30 +4,16 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 
 public class Robot extends TimedRobot {
-    Timer timer;
-    Timer outputTimer;
     public Robot() {
         super.setPeriod(0.01);
-        timer = new Timer();
-        outputTimer = new Timer();
     }
 
     @Override
     public void robotInit() {
-        outputTimer.start();
     }
 
     @Override
     public void robotPeriodic() {
-        if(outputTimer.get() > 0.5) {
-            System.out.println(timer.get());
-            outputTimer.stop();
-            outputTimer.reset();
-            outputTimer.start();
-        }
-        timer.stop();
-        timer.reset();
-        timer.start();
     }
 
     @Override
@@ -52,6 +38,10 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopPeriodic() {
+    }
+
+    @Override
+    public void testInit() {
     }
 
     @Override
